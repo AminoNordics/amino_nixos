3ffcfb501fb363777cf3333f079b33208343f2b2
Aksel Stadler <aksel@stadler.no>
2025-04-06 14:52:27 +0200

    Refactor Digital Ocean installer configuration in installer_do.nix to streamline imports and consolidate system settings, including SSH access and networking configurations.

--- hosts/installer_do.nix
+++ hosts/installer_do.nix
@@ -1,42 +1,28 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  config = {
-    imports = [ <nixpkgs/nixos/modules/virtualisation/digital-ocean-image.nix> ];
-  };
-in
-(pkgs.nixos config).digitalOceanImage
+{ config, lib, pkgs, ... }:
 
-updates = {
-  environment.systemPackages = with pkgs; [
-    curl
-    git
-    gvisor
-    overmind
-    docker
-    kubo
-    grpcurl
+{
+  imports = [
+    "${pkgs.path}/nixos/modules/virtualisation/digital-ocean-image.nix"
   ];
-  
-    
-    # Basic system configuration for DigitalOcean
-    system.stateVersion = "23.11";
-    
-    # Setup SSH access for remote login
-    services.openssh = {
-      enable = true;
-      settings.PermitRootLogin = "yes"; # Important for DO initial login
-      settings.PasswordAuthentication = false;
-    };
-    
-    # Users configuration
-    users.users.root.openssh.authorizedKeys.keys = [
-      # Add your SSH public key here
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExample/Key/Replace/This"
-    ];
-    
-    # Network configuration for DigitalOcean
-    networking = {
-      hostName = "nixos-do";
-      firewall.allowedTCPPorts = [ 22 80 443 8080 ];
-    };
-} 
+
+  environment.systemPackages = with pkgs; [
+    curl git gvisor overmind docker kubo grpcurl
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PermitRootLogin = "yes";
+    settings.PasswordAuthentication = false;
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExample/Key/Replace/This"
+  ];
+
+  networking = {
+    hostName = "nixos-do";
+    firewall.allowedTCPPorts = [ 22 80 443 8080 ];
+  };
+
+  system.stateVersion = "23.11";
+}
