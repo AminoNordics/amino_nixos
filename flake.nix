a7d9c7f4e3f6726b71075facc42eb508f67ce1a6
Aksel Stadler <aksel@stadler.no>
2025-04-06 14:50:32 +0200

    sads

--- flake.nix
+++ flake.nix
@@ -1,43 +1,48 @@
 {
-  description = "NixOS configuration with dev service and DigitalOcean installer";
+  description = "Amino NixOS configurations: dev, prod, installer";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     agenix.url = "github:ryantm/agenix";
     crs-server.url = "path:../crs-server";
+
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    crs-server.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, agenix, crs-server, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        # DigitalOcean installer package
-        packages.installer_do = import ./hosts/installer_do.nix {
-          inherit pkgs;
-          system = system;
-        };
-        # Default package is the installer
-        defaultPackage = self.packages.${system}.installer_do;
-      }
-    ) // {
-      # NixOS configurations
-      nixosConfigurations = {
-        # Development system configuration
-        dev = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = {
-            crs_server = crs-server;
-          };
-          modules = [
-            agenix.nixosModules.default
-            ./hosts/dev.nix
-            ./modules/agenix.nix
-            ./modules/postgres.nix
-            ./modules/crs_server.nix
-            ./modules/caddy.nix
-          ];
-        };
+
+  outputs = { self, nixpkgs, flake-utils, agenix, crs-server, ... }: {
+    nixosConfigurations = {
+      dev = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { crs_server = crs-server; };
+        modules = [
+          agenix.nixosModules.default
+          ./hosts/dev.nix
+          ./modules/agenix.nix
+          ./modules/postgres.nix
+          ./modules/crs_server.nix
+          ./modules/caddy.nix
+        ];
       };
+
+      installer_do = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/installer_do.nix
+        ];
+      };
+
+      # You can add prod_worker and prod_database like this later:
+      # prod_worker = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   modules = [ ./hosts/prod_worker.nix ./modules/crs_server.nix ];
+      # };
+
+      # prod_database = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   modules = [ ./hosts/prod_database.nix ./modules/postgres.nix ];
+      # };
     };
+  };
 }
--- hosts/installer_do.nix
+++ hosts/installer_do.nix
@@ -1,13 +1,22 @@
-{ pkgs, system }:
-
+{ pkgs ? import <nixpkgs> {} }:
 let
-  lib = pkgs.lib;
+  config = {
+    imports = [ <nixpkgs/nixos/modules/virtualisation/digital-ocean-image.nix> ];
+  };
+in
+(pkgs.nixos config).digitalOceanImage
+
+updates = {
+  environment.systemPackages = with pkgs; [
+    curl
+    git
+    gvisor
+    overmind
+    docker
+    kubo
+    grpcurl
+  ];
   
-  # Define the DigitalOcean installer configuration
-  doInstallerConfig = { modulesPath, ... }: {
-    imports = [
-      "${modulesPath}/virtualisation/digital-ocean-image.nix"
-    ];
     
     # Basic system configuration for DigitalOcean
     system.stateVersion = "23.11";
@@ -30,61 +39,4 @@ let
       hostName = "nixos-do";
       firewall.allowedTCPPorts = [ 22 80 443 8080 ];
     };
-    
-    # Add the nixos-reinstall functionality for pointing to the dev configuration
-    environment.systemPackages = with pkgs; [
-      git
-      nix-output-monitor
-      
-      # Custom nixos-reinstall script
-      (writeScriptBin "nixos-reinstall" ''
-        #!${pkgs.bash}/bin/bash
-        set -e
-        
-        echo "Reinstalling NixOS configuration..."
-        if [ -z "$1" ]; then
-          echo "Usage: nixos-reinstall <flake-uri>#<config>"
-          echo "Example: nixos-reinstall github:yourusername/yourrepo#dev"
-          exit 1
-        fi
-        
-        FLAKE_URI="$1"
-        
-        # Install the specified configuration
-        ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake "$FLAKE_URI" --use-remote-sudo
-        
-        echo "NixOS configuration installed successfully!"
-      '')
-    ];
-  };
-  
-  # Build the DigitalOcean image
-  doImage = (pkgs.nixos [doInstallerConfig]).digitalOceanImage;
-  
-in pkgs.runCommand "installer-do" {} ''
-  mkdir -p $out/nix-support
-  ln -s ${doImage}/nixos.img $out/nixos-do.img
-  echo "file img $out/nixos-do.img" > $out/nix-support/hydra-build-products
-  
-  # Add README instructions
-  cat > $out/README.md << EOF
-  # NixOS DigitalOcean Installer
-  
-  This package contains a NixOS image for DigitalOcean.
-  
-  ## Usage
-  
-  1. Upload the \`nixos-do.img\` file to DigitalOcean as a custom image
-  2. Create a droplet using this custom image
-  3. SSH into the droplet
-  4. Run the following command to install the development environment:
-     \`\`\`
-     nixos-reinstall github:yourusername/yourrepo#dev
-     \`\`\`
-  
-  ## Note
-  
-  Replace \`github:yourusername/yourrepo\` with the URL of your Git repository
-  containing this flake configuration.
-  EOF
-''
+} 
