7693e6aab9be786a914d391d928eff3550127f56
Aksel Stadler <aksel@stadler.no>
2025-04-07 21:00:12 +0200

    as

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 {
   
     "postgres_password.age".publicKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMV7phgYvZhQOd2wQnekoDH7m6GUlrappx3zNvGt34j9 root@nixos-do"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCk2+KRogVgvwc4o6Jb3oW0VM/5V+/lq03NfbnLxewt root@nixos-do"
+
   ];
 }
