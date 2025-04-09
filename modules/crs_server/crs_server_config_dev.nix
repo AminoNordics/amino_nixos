ffef89c5b33eb480ef57062ec54bac5e202a9a72
Aksel Stadler <aksel@stadler.no>
2025-04-09 18:57:50 +0200

    Update crs_server_config_dev.nix to correct PostgreSQL password file path, ensuring accurate configuration for the development environment.

--- modules/crs_server/crs_server_config_dev.nix
+++ modules/crs_server/crs_server_config_dev.nix
@@ -3,7 +3,7 @@
 {
   # Required for secrets
   age.secrets.postgres_password_dev = {
-    file = ../secrets/postgres_password_dev.age;
+    file = ../secrets/postgres_password.age;
     mode = "0400";
     owner = "crs";
   };
